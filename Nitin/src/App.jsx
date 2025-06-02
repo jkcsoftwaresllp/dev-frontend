dde3ff683d536ca225757357e6f9850b6314ea19
NITIN <nitinsharmaop828@gmail.com>
2025-06-02 17:17:38 +0530

    Activity 1

---
+++ Nitin/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FeaturesSection from './components/FeaturesSection/FeaturesSection';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <FeaturesSection />
+    </div>
+  );
+};
+
+export default App;
