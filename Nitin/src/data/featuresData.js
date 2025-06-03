8592a5500b7f30b306355a4078b7ee9bc404380a
Nitin-777 <nitinsharmaop828@gmail.com>
2025-06-03 10:53:09 +0530

    Make reusable component (#4)
    * first
    
    * Activity 1
    
    * final

---
+++ Nitin/src/data/featuresData.js
@@ -0,0 +1,46 @@
+// src/data/featuresData.js
+export const featuresData = [
+  {
+    id: 1,
+    title: "Real Time Stock Tracking",
+    description: "Monitor real-time stock trends for smarter decisions",
+    isHighlighted: true
+  },
+  {
+    id: 2,
+    title: "GST Billing",
+    description: "Effortless GST billing and inventory management"
+  },
+  {
+    id: 3,
+    title: "Reports",
+    description: "Generate detailed reports for orders, sales, and more"
+  },
+  {
+    id: 4,
+    title: "Dedicated Customer Support",
+    description: "Reliable customer support whenever you need it"
+  },
+  {
+    id: 5,
+    title: "Unlimited Warehouse",
+    description: "Manage multiple warehouses with unlimited capacity"
+  },
+  {
+    id: 6,
+    title: "Expiry Alert",
+    description: "Get timely expiry alerts to avoid stock wastage"
+  },
+  {
+    id: 7,
+    title: "Discount & Offers",
+    description: "Easily apply discounts and manage special offers"
+  },
+  {
+    id: 8,
+    title: "Centralized Dashboard",
+    description: "Access all your business data through a single dashboard"
+  }
+];
+
+export default featuresData;
